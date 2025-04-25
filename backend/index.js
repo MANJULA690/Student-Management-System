3183934023e647ee82a443718574fc8571b51332
Ramavath Manjula <manjuramavath12@gmail.com>
2025-04-25 08:18:34 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,6 @@ const app = express();
 app.use(cors( ));
 app.use(express.json());
 
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(() => console.log('MongoDB Connected'))
-//   .catch(err => console.error(err));
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
